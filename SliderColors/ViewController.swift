7707cad3b3595322075c5c81d47e6baffa8dfe47
mariya.shabanova <mariya.shabanova@vk.team>
2023-11-19 23:44:55 +0300

    all done

--- SliderColors/ViewController.swift
+++ SliderColors/ViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
     
     @IBOutlet var colorSpace: UIView!
     
@@ -19,11 +19,39 @@ class ViewController: UIViewController {
     @IBOutlet var greenSlider: UISlider!
     @IBOutlet var blueSlider: UISlider!
     
+    private var currentRedPersent = 0.51
+    private var currentGreenPersent = 0.51
+    private var currentBluePersent = 0.51
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         colorSpace.layer.cornerRadius = 10
+        changeColor()
     }
 
-
+    @IBAction func redSliderChanged() {
+        let currentRedPersentText =  String(format: "%.2f", redSlider.value)
+        redPercent.text = currentRedPersentText
+        currentRedPersent = Double(currentRedPersentText)!
+        changeColor()
+    }
+    
+    @IBAction func greenSliderChanged() {
+        let currentGreenPersentText = String(format: "%.2f", greenSlider.value)
+        greenPercent.text = currentGreenPersentText
+        currentGreenPersent = Double(currentGreenPersentText)!
+        changeColor()
+    }
+    
+    @IBAction func blueSliderChanged() {
+        let currentBluePersentText = String(format: "%.2f", blueSlider.value)
+        bluePercent.text = currentBluePersentText
+        currentBluePersent = Double(currentBluePersentText)!
+        changeColor()
+    }
+    
+    private func changeColor() {
+        colorSpace.backgroundColor = UIColor(red: currentRedPersent, green: currentGreenPersent, blue: currentBluePersent, alpha: 1.0)
+    }
 }
 
