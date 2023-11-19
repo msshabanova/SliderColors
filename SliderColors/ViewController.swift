4065f4e5fb08ff547e343adf70e0c30e17e801dd
mariya.shabanova <mariya.shabanova@vk.team>
2023-11-19 23:06:24 +0300

    fix ui

--- SliderColors/ViewController.swift
+++ SliderColors/ViewController.swift
@@ -8,10 +8,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var colorSpace: UIView!
+    
+    @IBOutlet var redPercent: UILabel!
+    @IBOutlet var greenPercent: UILabel!
+    @IBOutlet var bluePercent: UILabel!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        colorSpace.layer.cornerRadius = 10
     }
 
 
